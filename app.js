5c8b0a46b9efe5e170929a420533c579e3acfb6e
Bill <ngongangsomen@gmail.com>
2020-11-01 13:45:04 +0100

    med.sync

--- app.js
+++ app.js
@@ -1,76 +1,112 @@
-const app = require('express')()
-const server = require('http').createServer(app)
-const io = require('socket.io')(server)
+// import {routerOptions} from "../client/.nuxt/router";
 
-const usersDB = require('./utils/users')()
-const Message = require('./Models/Message')()
+const app = require("express")();
+const server = require("http").createServer(app);
+const io = require("socket.io")(server);
+
+const usersDB = require("./utils/users")();
+const Message = require("./Models/Message")();
+
+const sharedRooms = {
+  doctorsPatients: 'doctorsPatients',
+  doctorsWeb: 'doctorsWeb',
+  patientsWeb: 'patientsWeb',
+}
 
 // when there is a connection
-io.on('connection', (socket) => {
+io.on("connection", (socket) => {
   // Create User
-  socket.on('createUser', (user) => {
+  socket.on("createUser", (user) => {
     //console.log("Create user", user);
-
+    
     usersDB.addUser({
       ...user,
       id: socket.id,
-    })
-    console.log('soccet id', socket.id)
-    return { id: socket.id }
-  })
-
-  // Join a Room
-  socket.on('joinRoom', ({ name, room }) => {
-    //console.log("Join Room", name, room);
-
-    socket.join(room)
-    io.to(room).emit('updateUsers', usersDB.getUsersByRoom(room))
-    socket.emit('newMessage', new Message('admin', `Hello, ${name}`))
-    socket.broadcast
-      .to(room)
-      .emit(
-        'newMessage',
-        new Message('admin', `User ${name} connected to chat`)
-      )
-  })
-
-  // Create Message
-  socket.on('createMessage', ({ id, msg }) => {
-    // console.log("Create Message", msg);
-    const user = usersDB.getUser(id)
-    if (user) {
-      io.to(user.room).emit('newMessage', new Message(user.name, msg, id))
+    });
+    console.log("socket id", socket.id);
+    console.log(user);
+    return { id: socket.id };
+  });
+  
+  socket.on("connectPractitioner", ({practitionerId, roomId, patientId}) => {
+    //1. get the socketIo id of the doctor
+    const doctor = usersDB.getUserByPublicId(practitionerId)
+    if (doctor){
+      // update patient (current requester)
+      const patient = usersDB.getUserByPublicId(patientId)
+      patient.doctorsList.push(doctor)
+      patient.doctorCurrent = doctor
+      patient.doctorCurrent.dateConnection = new Date()
+      
+      usersDB.updateUser(patient.id, patient)
+      
+      // update doctor
+      doctor.patientsList.push(patient)
+      doctor.patientCurrent = patient
+      doctor.patientCurrent.dateConnection = new Date()
+      
+      // patient join roomId
+      // socket.join(roomId);
+      // 2. send a specific message
+      delete patient.doctorsList
+      delete patient.doctorCurrent
+      io.to(doctor.id).emit("connectPractitioner", patient);
     }
-  })
-
+  });
+  
+  // // Join a Room
+  socket.on("joinRoom", ({ name, room }) => {
+    console.log("Join Room", name, room);
+    
+    socket.join(room);
+    io.to(room).emit("updateUsers", usersDB.getUsersByRoom(room));
+    socket.emit("newMessage", new Message("admin", `Hello, ${name}`));
+    socket.broadcast
+        .to(room)
+        .emit(
+            "newMessage",
+            new Message("admin", `User ${name} connected to chat`)
+        );
+  });
+  
+  // Create Message
+  socket.on("createMessage", ({ id, msg }) => {
+    // console.log("Create Message", msg);
+    const user = usersDB.getUser(id);
+    if (user) {
+      io.to(id).emit("newMessage", new Message(user.name, msg, id));
+      io.to(user.room).emit("newMessage", new Message(user.name, msg, id));
+    }
+  });
+  
   // Set Typing Status
-  socket.on('setTypingStatus', ({ room, typingStatus, id }) => {
+  socket.on("setTypingStatus", ({ room, typingStatus, id }) => {
     //console.log("set TyPing Status", typingStatus, room);
-
-    usersDB.setTypingStatus(id, typingStatus)
-    io.to(room).emit('updateUsers', usersDB.getUsersByRoom(room))
-  })
-
-  const exitEvents = ['leftChat', 'disconnect']
-
+    
+    usersDB.setTypingStatus(id, typingStatus);
+    io.to(room).emit("updateUsers", usersDB.getUsersByRoom(room));
+  });
+  
+  const exitEvents = ["leftChat", "disconnect"];
+  
   exitEvents.forEach((event) => {
     socket.on(event, () => {
-      const id = socket.id
-      const user = usersDB.getUser(id)
-      if (!user) return
-      const { room, name } = user
-      usersDB.removeUser(id)
-      socket.leave(room)
-      io.to(room).emit('updateUsers', usersDB.getUsersByRoom(room))
+      const id = socket.id;
+      const user = usersDB.getUser(id);
+      if (!user) return;
+      const { room, name } = user;
+      usersDB.removeUser(id);
+      socket.leave(room);
+      io.to(room).emit("updateUsers", usersDB.getUsersByRoom(room));
       io.to(room).emit(
-        'newMessage',
-        new Message('admin', `User ${name} left chat`)
-      )
-    })
-  })
-})
+          "newMessage",
+          new Message("admin", `User ${name} left chat`)
+      );
+    });
+  });
+});
 
 module.exports = {
   app,
   server,
-}
+};
--- utils/users.js
+++ utils/users.js
@@ -2,23 +2,37 @@ class UsersDB {
   constructor() {
     this.users = [];
   }
-
+  
   addUser(user) {
+    user.dateAdd = new Date()
     this.users = [...this.users, user];
   }
-
+  
   getUser(id) {
     return this.users.find((user) => user.id === id);
   }
-
+  
+  updateUser(id, update) {
+    // const user = this.getUser(id)
+    update.dateUpdate = new Date()
+    update.id = id
+    const index = this.users.findIndex((user) => user.id === id);
+    this.users[index] = update;
+    return update
+  }
+  
+  getUserByPublicId(_id) {
+    return this.users.find((user) => user._id === _id);
+  }
+  
   getUsersByRoom(room) {
     return this.users.filter((user) => user.room === room);
   }
-
+  
   removeUser(id) {
     this.users = this.users.filter((user) => user.id !== id);
   }
-
+  
   setTypingStatus(id, typingStatus) {
     const index = this.users.findIndex((user) => user.id === id);
     this.users[index].typingStatus = typingStatus;
