4eb8437ba3388610d253bebb71ebc2a424c8f47e
gnopor <mayoublaise@gmail.com>
2020-11-11 19:10:09 -0500

    add nemEntry listener

--- app.js
+++ app.js
@@ -8,17 +8,17 @@ const usersDB = require("./utils/users")();
 const Message = require("./Models/Message")();
 
 const sharedRooms = {
-  doctorsPatients: 'doctorsPatients',
-  doctorsWeb: 'doctorsWeb',
-  patientsWeb: 'patientsWeb',
-}
+  doctorsPatients: "doctorsPatients",
+  doctorsWeb: "doctorsWeb",
+  patientsWeb: "patientsWeb",
+};
 
 // when there is a connection
 io.on("connection", (socket) => {
   // Create User
   socket.on("createUser", (user) => {
     //console.log("Create user", user);
-    
+
     usersDB.addUser({
       ...user,
       id: socket.id,
@@ -27,13 +27,13 @@ io.on("connection", (socket) => {
     console.log(user);
     return { id: socket.id };
   });
-  
-  socket.on("connectPractitioner", ({practitionerId, roomId, patientId}) => {
+
+  socket.on("connectPractitioner", ({ practitionerId, roomId, patientId }) => {
     //1. get the socketIo id of the doctor
-    const doctor = usersDB.getUserByPublicId(practitionerId)
-    if (doctor){
+    const doctor = usersDB.getUserByPublicId(practitionerId);
+    if (doctor) {
       // update patient (current requester)
-      const patient = usersDB.getUserByPublicId(patientId)
+      const patient = usersDB.getUserByPublicId(patientId);
       // patient.doctorsList.push(doctor)
       // patient.doctorCurrent = doctor
       // patient.doctorCurrent.dateConnection = new Date()
@@ -53,13 +53,13 @@ io.on("connection", (socket) => {
       io.to(doctor.id).emit("connectPractitioner", patient);
     }
   });
-  
-  socket.on("validatePractitioner", ({practitionerId, patientId}) => {
+
+  socket.on("validatePractitioner", ({ practitionerId, patientId }) => {
     //1. get the socketIo id of the doctor
-    const doctor = usersDB.getUserByPublicId(practitionerId)
-    if (doctor){
+    const doctor = usersDB.getUserByPublicId(practitionerId);
+    if (doctor) {
       // update patient (current requester)
-      const patient = usersDB.getUserByPublicId(patientId)
+      const patient = usersDB.getUserByPublicId(patientId);
       // patient.doctorsList.push(doctor)
       // patient.doctorCurrent = doctor
       // patient.doctorCurrent.dateConnection = new Date()
@@ -79,48 +79,54 @@ io.on("connection", (socket) => {
       io.to(doctor.id).emit("validatePractitioner", patient);
     }
   });
-  
-  socket.on("handShakePartners", ({practitionerId, patientId}) => {
+
+  socket.on("handShakePartners", ({ practitionerId, patientId }) => {
     //1. get the socketIo id of the doctor
-    const doctor = usersDB.getUserByPublicId(practitionerId)
-    if (doctor){
+    const doctor = usersDB.getUserByPublicId(practitionerId);
+    if (doctor) {
       // update patient (current requester)
-      const patient = usersDB.getUserByPublicId(patientId)
-      patient.doctorsList.push(doctor)
-      patient.doctorCurrent = doctor
-      patient.doctorCurrent.dateConnection = new Date()
-      
-      usersDB.updateUser(patient.id, patient)
-      
+      const patient = usersDB.getUserByPublicId(patientId);
+      patient.doctorsList.push(doctor);
+      patient.doctorCurrent = doctor;
+      patient.doctorCurrent.dateConnection = new Date();
+
+      usersDB.updateUser(patient.id, patient);
+
       // update doctor
-      doctor.patientsList.push(patient)
-      doctor.patientCurrent = patient
-      doctor.patientCurrent.dateConnection = new Date()
-      
+      doctor.patientsList.push(patient);
+      doctor.patientCurrent = patient;
+      doctor.patientCurrent.dateConnection = new Date();
+
       // patient join roomId
       // socket.join(roomId);
       // 2. send a specific message
-      delete patient.doctorsList
-      delete patient.doctorCurrent
+      delete patient.doctorsList;
+      delete patient.doctorCurrent;
       io.to(patient.id).emit("handShakePartners");
     }
   });
-  
+
+  // // handle new entry
+  socket.on("emitSignal", (newEntry) => {
+    const entry = newEntry;
+    io.emit("setNewEntry", entry);
+  });
+
   // // Join a Room
   socket.on("joinRoom", ({ name, room }) => {
     console.log("Join Room", name, room);
-    
+
     socket.join(room);
     io.to(room).emit("updateUsers", usersDB.getUsersByRoom(room));
     socket.emit("newMessage", new Message("admin", `Hello, ${name}`));
     socket.broadcast
-        .to(room)
-        .emit(
-            "newMessage",
-            new Message("admin", `User ${name} connected to chat`)
-        );
+      .to(room)
+      .emit(
+        "newMessage",
+        new Message("admin", `User ${name} connected to chat`)
+      );
   });
-  
+
   // Create Message
   socket.on("createMessage", ({ id, msg }) => {
     // console.log("Create Message", msg);
@@ -130,17 +136,17 @@ io.on("connection", (socket) => {
       io.to(user.room).emit("newMessage", new Message(user.name, msg, id));
     }
   });
-  
+
   // Set Typing Status
   socket.on("setTypingStatus", ({ room, typingStatus, id }) => {
     //console.log("set TyPing Status", typingStatus, room);
-    
+
     usersDB.setTypingStatus(id, typingStatus);
     io.to(room).emit("updateUsers", usersDB.getUsersByRoom(room));
   });
-  
+
   const exitEvents = ["leftChat", "disconnect"];
-  
+
   exitEvents.forEach((event) => {
     socket.on(event, () => {
       const id = socket.id;
@@ -151,8 +157,8 @@ io.on("connection", (socket) => {
       socket.leave(room);
       io.to(room).emit("updateUsers", usersDB.getUsersByRoom(room));
       io.to(room).emit(
-          "newMessage",
-          new Message("admin", `User ${name} left chat`)
+        "newMessage",
+        new Message("admin", `User ${name} left chat`)
       );
     });
   });
