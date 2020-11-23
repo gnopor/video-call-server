50e4bcaf67dbe4fd75a056493160e7b87ac53d58
gnopor <mayoublaise@gmail.com>
2020-11-22 20:13:52 -0500

    first commit

--- index.js
+++ index.js
@@ -1,10 +1,39 @@
-if (process.env.NODE_ENV !== "production") {
-  require("dotenv").config();
-}
+const app = require("express")();
+const server = require("http").createServer(app);
+const io = require("socket.io")(server);
 
-const { app, server } = require("./app");
+const port = process.env.PORT || 5000;
 
-const port = process.env.PORT;
+// when there is a connection
+io.on("connection", (socket) => {
+  console.log(socket.id);
+
+  // brodadcast to all others users that we are connected
+  socket.broadcast.emit("add-users", {
+    users: [socket.id],
+  });
+
+  // listen to make-offer and emmit offer-made to the selecter peer
+  socket.on("make-offer", function (data) {
+    socket.to(data.to).emit("offer-made", {
+      offer: data.offer,
+      socket: socket.id,
+    });
+  });
+
+  // listen to make-answer and send answer back to the peer which create the offer
+  socket.on("make-answer", function (data) {
+    socket.to(data.to).emit("answer-made", {
+      socket: socket.id,
+      answer: data.answer,
+    });
+  });
+
+  // disconnect user and tell other user to remove it
+  socket.on("disconnect", () => {
+    io.emit("remove-user", socket.id);
+  });
+});
 
 async function start() {
   // Listen the server
